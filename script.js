a76ab6f44d03055fc572d6ba68b36cc1d1f1be38
Archisman <archisman.mukherjee02@gmail.com>
2023-01-29 22:27:34 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,52 @@
+'use strict';
+let randomNumber = Math.trunc(Math.random() * 20) + 1;
+const message = document.querySelector('.message');
+let score = 20;
+let highScore = 0;
+
+const displayMessage = function (message) {
+  document.querySelector('.message').textContent = message;
+};
+
+document.querySelector('.check').addEventListener('click', function () {
+  const guess = Number(document.querySelector('.guess').value);
+  console.log(guess);
+  if (!guess) {
+    console.log('Enter a number to play the game!');
+    displayMessage('⛔ No number!'); // Get emojis using Windows + .
+  } else if (guess !== randomNumber) {
+    if (score > 1) {
+      console.log('Not correct!');
+      displayMessage(guess > randomNumber ? '📈 Too high!' : '📉 Too low!');
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      displayMessage('💥 You lost the game!');
+      document.querySelector('.score').textContent = 0;
+    }
+  } else if (guess === randomNumber) {
+    console.log('Correct number!');
+    document.querySelector('.number').textContent = randomNumber;
+
+    displayMessage('🎉 Correct number!');
+    document.querySelector('body').style.backgroundColor = '#60b347';
+
+    document.querySelector('.number').style.width = '30rem';
+
+    if (score > highScore) {
+      highScore = score;
+      document.querySelector('.highscore').textContent = highScore;
+    }
+  }
+});
+
+document.querySelector('.again').addEventListener('click', function () {
+  score = 20;
+  randomNumber = Math.trunc(Math.random() * 20) + 1;
+  message.textContent = 'Start guessing...';
+  document.querySelector('.score').textContent = score;
+  document.querySelector('body').style.backgroundColor = '#222';
+  document.querySelector('.number').style.width = '15rem';
+  document.querySelector('.number').textContent = '?';
+  document.querySelector('.guess').value = '';
+});
